e63a84ac533f2f799acb9bdfedd39cc65794ece7
facetoe <willcoe@westnet.com.au>
2012-10-24 19:32:59 +0700

    Added library for ncurses functions.

---
+++ lib/libcurses_functions.h
@@ -0,0 +1,13 @@
+/*
+ * libcurses_functions.h
+ *
+ *  Created on: 23/10/2012
+ *      Author: fragmachine
+ */
+
+#ifndef LIBCURSES_FUNCTIONS_H_
+#define LIBCURSES_FUNCTIONS_H_
+
+
+
+#endif /* LIBCURSES_FUNCTIONS_H_ */
